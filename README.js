33ed55341a3a8d51a375a01514c5f224de0241b4
Mashinegal <153958287+Mashinegal@users.noreply.github.com>
2024-09-12 14:05:18 +0200

    Update README.md

--- README.js
+++ README.js
@@ -1,24 +1,2 @@
-
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <div className="main-container">
-        <Header />
-        <div className="visual-section">
-          {/* This would be where your 3D illustration goes */}
-          <div className="visual-element">
-            <p>3D Model here</p>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
-}
-
-export default App;
+cd cleverbot-ui
+npm start
